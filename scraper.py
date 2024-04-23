b3a000eba02d8056916dcb352ed7b55550fe2040
Alex Meng <alex2002c@gmail.com>
2024-04-22 23:19:02 -0700

    updated extract next links and is valid

--- scraper.py
+++ scraper.py
@@ -1,6 +1,9 @@
 import re
-from urllib.parse import urlparse, urlunparse
+from urllib.parse import urlparse, urlunparse, urljoin
 from bs4 import BeautifulSoup
+
+uniqueURLS = set()
+
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
@@ -16,14 +19,24 @@ def extract_next_links(url, resp):
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
     extracted_links = []
-    if resp.status == 200 and has_high_content(resp):
+
+    if resp.status == 200 and has_high_content(resp): #checks for valid response and if it has enough textual content
         
         soup = BeautifulSoup(resp.raw_response, 'html.parser')
         for link in soup.find_all('a'):
-            href = link.get('href')
-            extracted_links.append(href)
-    if resp.status == 302 or resp.status == 301:
-        pass
+            tempURL = link.get('href')
+            if tempURL:
+                clean_url = defragment_url(tempURL) #removes fragmentation
+                if clean_url not in extracted_links:
+                    extracted_links.append(clean_url)
+
+    if resp.status == 302 or resp.status == 301: #handles directs
+        location_header = resp.headers.get('Location')
+        if location_header:
+            redirect_url = urljoin(url, location_header)
+            if is_valid(redirect_url):
+                extracted_links.append(redirect_url)
+
     return extracted_links
 
 def is_valid(url):
@@ -31,14 +44,13 @@ def is_valid(url):
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
     try:
-        new_url = defragment_url(url)
+        parsed = urlparse(url)
         valid_domains = ["ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu"]
-        valid_paths = ["/"]
-
-        if new_url.scheme not in set(["http", "https"]):
+        valid_paths = ['/']
+        if parsed.scheme not in set(["http", "https"]) or (url.find("?") != -1) or (url.find("&") != -1):
             return False
         
-        if new_url.netloc.endswith(tuple(valid_domains)) and any(new_url.path.startswith(path) for path in valid_paths):
+        if parsed.netloc.endswith(tuple(valid_domains)) and any(parsed.path.startswith(path) for path in valid_paths):
 
             if re.match(
                 r".*\.(css|js|bmp|gif|jpe?g|ico"
@@ -48,21 +60,27 @@ def is_valid(url):
                 + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
                 + r"|epub|dll|cnf|tgz|sha1"
                 + r"|thmx|mso|arff|rtf|jar|csv"
-                + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", new_url.path.lower()):
+                + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower()):
                 return False
-            return True
+            if url in uniqueURLS:
+                return False
+            else:
+                uniqueURLS.add(url)
+                return True
 
     except TypeError:
-        print ("TypeError for ", new_url)
+        print ("TypeError for ", parsed)
         raise
 
 def defragment_url(url):
-    parsed_url = urlparse(url)
-    cleaned_url = parsed_url._replace(fragment='')
-    return urlunparse(cleaned_url)
+    # removes the fragment section of url and returns the url without it
+    parsed_url = urlparse(url)._replace(fragment='')
+    return urlunparse(parsed_url)
 
 
 def has_high_content(response):
+
+    """checks if response has enough textual content by comparing the word to html tag ratio to a given threshold"""
 
     if response.raw_response:
         html_content = response.raw_response.content
