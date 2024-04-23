b61b5208a2b3bcf5b6a6fff5b557267a659715d1
RaymondYan38 <raymondyan17@gmail.com>
2024-04-22 23:43:43 -0700

    all of our code combined thus far

--- scraper.py
+++ scraper.py
@@ -1,11 +1,11 @@
 import os
 import re
-from urllib.parse import urlparse, urlencode, parse_qsl, quote, unquote
+from urllib.parse import urlparse, urlunparse, urljoin, urlencode, parse_qsl, quote, unquote
+from urllib import robotparser
 from bs4 import BeautifulSoup
 from collections import defaultdict
 import logging
 import logging_config
-
 
 NON_HTML_EXTENSIONS_PATTERN = re.compile(
     r"\.(css|js|bmp|gif|jpe?g|ico"
@@ -199,9 +199,52 @@ stop_words = ["a",
 
 word_to_occurances = defaultdict(int)
 
+uniqueURLS = set()
+robotstxtdict = {}
+
 def scraper(url, resp):
-    links = extract_next_links(url, resp)
-    return [link for link in links if is_valid(link)]
+    can_crawl = politeness(url) 
+    if can_crawl:
+        links = extract_next_links(url, resp)
+        return [link for link in links if is_valid(link)]
+    else:
+        return []
+
+def politeness(url):
+    parsed_url = urlparse(url)
+    domain = parsed_url.hostname
+    can_crawl = True
+
+    # Check if the main domain's robots.txt has already been checked
+    if domain in robotstxtdict:
+        # Check if the current URL is in the list of subdomains that can't be crawled
+        if url in robotstxtdict[domain]['subdomains']:
+            can_crawl = False
+            return can_crawl
+        
+        crawl_delay = robotstxtdict[domain]['crawl_delay']
+    else:
+        rp = robotparser.RobotFileParser()
+        rp.set_url(f"{parsed_url.scheme}://{domain}/robots.txt")
+        
+        try:
+            rp.read()
+            # Check if the domain has a robots.txt file
+            if not rp.can_fetch("*", url):
+                can_crawl = False
+                return can_crawl
+            
+            crawl_delay = rp.crawl_delay("*")
+            # Cache the crawl delay and subdomains in robotstxtdict
+            robotstxtdict[domain] = {
+                'crawl_delay': crawl_delay,
+                'subdomains': set()
+            }
+        except Exception as e:
+            # Handle exceptions, e.g., network errors, missing robots.txt
+            pass
+
+    return can_crawl
 
 def extract_next_links(url, resp):
     # Implementation required.
@@ -213,8 +256,39 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    extracted_links = []
+    base_url = resp.request.url #original url of the pages
 
+    if resp.status == 200 and has_high_content(resp): #checks for valid response and if it has enough textual content
+        
+        soup = BeautifulSoup(resp.raw_response, 'html.parser')
+        for link in soup.find_all('a'):
+            tempURL = link.get('href')
+            if tempURL:
+                clean_url = urljoin(base_url, tempURL) #resolves relative URLs
+                clean_url = defragment_url(clean_url) #removes fragmentation
+
+                if clean_url not in extracted_links:
+                    extracted_links.append(clean_url)
+
+    if resp.status == 302 or resp.status == 301: #handles redirects
+        location_header = resp.headers.get('Location')
+        if location_header:
+            redirect_url = urljoin(base_url, location_header)
+            if is_valid(redirect_url) and has_high_content(redirect_url):
+                extracted_links.append(redirect_url)
+
+    return extracted_links
+
+def defragment_url(url):
+    # removes the fragment section of url and returns the url without it
+    parsed_url = urlparse(url)._replace(fragment='')
+    return urlunparse(parsed_url)
+
+"""URLs can represent the same page in multiple ways. For example, http://example.com, 
+http://example.com/, http://example.com/index.html, and http://example.com/? could all point to the same resource. Implemened URL 
+canonicalization to standardize URLs and avoid crawling the same content multiple times.
+"""
 def canonicalize_url(url):
     # Parse the URL
     parsed = urlparse(url)
@@ -269,3 +343,22 @@ def is_valid(url):
     except TypeError:
         print("TypeError for ", parsed)
         raise
+
+def has_high_content(response):
+
+    """checks if response has enough textual content by comparing the word to html tag ratio to a given threshold"""
+
+    if response.raw_response:
+        html_content = response.raw_response.content
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        text = soup.get_text()
+        word_count = len(text.split())
+        tag_count = tag_count = len(soup.find_all())
+
+        threshold = 50
+
+        return (word_count/tag_count) > 50
+        
+
+    return False
